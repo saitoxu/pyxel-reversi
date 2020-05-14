f66066d0bb646c420252b6a3cb85145e09f4b066
Aiko Saito <aikosaito@AikonoMacBook-Air.local>
2020-05-14 22:26:51 +0900

    initとgetを作りました

--- lib/board.py
+++ lib/board.py
@@ -1,9 +1,24 @@
 class Board:
     def __init__(self):
-        pass
+        import itertools
+        ROWS = ['1', '2', '3', '4', '5', '6', '7', '8']
+        COLS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        RCtup = tuple(itertools.product(ROWS, COLS))
+        RCdic = {}
+        for i in RCtup:
+            if i == ('4', 'e') or i == ('5', 'd'):
+                RCdic[i] = "black"
+            elif i == ('4', 'd') or i == ('5', 'e'):
+                RCdic[i] = "white"
+            else:
+                RCdic[i] = None
+        self.RCdic = RCdic
 
     def get(self, row, col):
-        return 'white'
+        self.row = row
+        self.col = col
+        gettup = (self.row , self.col)
+        return self.RCdic[gettup]
 
     def can_move(self, row, col, color):
         return True
@@ -16,3 +31,7 @@ class Board:
     
     def winner(self):
         return None
+
+if __name__ == "__main__":
+    board = Board()
+    print(board.get('5', 'd'))
