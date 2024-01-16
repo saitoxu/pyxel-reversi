136c3f3af2851ace1e73d132304eb7bde4ff7b54
Yosuke Saito <yosuke.saito1202@gmail.com>
2024-01-16 20:42:15 +0900

    change name

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# pyxel-othello
+# pyxel-reversi
 
-[pyxel](https://github.com/kitao/pyxel)を使ったオセロゲーム
+[pyxel](https://github.com/kitao/pyxel)を使ったリバーシ
 
 ## How to play
 
 [pyxelのインストール方法](https://github.com/kitao/pyxel#how-to-install)に従って事前にpyxelをインストールしてください。
 
 ```sh
-$ git clone https://github.com/saitoxu/pyxel-othello.git
-$ cd pyxel-othello
-$ python3 othello.py # or python othello.py
+$ git clone https://github.com/saitoxu/pyxel-reversi.git
+$ cd pyxel-reversi
+$ python3 reversi.py # or python reversi.py
 ```
--- reversi.py
+++ reversi.py
@@ -1,29 +1,30 @@
 import pyxel
+
+from lib.board import Board
 from lib.config import (
-    SCREEN_WIDTH,
-    SCREEN_HEIGHT,
-    SQUARE_SIZE,
-    ROWS,
     COLS,
+    PASS_BTN_H,
+    PASS_BTN_W,
     PASS_BTN_X,
     PASS_BTN_Y,
-    PASS_BTN_W,
-    PASS_BTN_H,
-    get_indices
+    ROWS,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    SQUARE_SIZE,
+    get_indices,
 )
-from lib.board import Board
 from lib.human_player import HumanPlayer
 from lib.random_player import RandomPlayer
 
 
-class Othello:
+class Reversi:
     def __init__(self):
-        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Pyxel Othello")
+        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Pyxel Reversi")
         pyxel.mouse(True)
         self.board = Board()
-        self.current = 'black'
-        self.black_player = HumanPlayer(pyxel, self.board, 'black')
-        self.white_player = RandomPlayer(pyxel, self.board, 'white')
+        self.current = "black"
+        self.black_player = HumanPlayer(pyxel, self.board, "black")
+        self.white_player = RandomPlayer(pyxel, self.board, "white")
         self.winner = None
         self.previous_frame_count = 0
         pyxel.run(self.update, self.draw)
@@ -32,14 +33,14 @@ class Othello:
         """
         手番の変更
         """
-        self.current = 'white' if self.current == 'black' else 'black'
+        self.current = "white" if self.current == "black" else "black"
         self.winner = self.board.winner()
 
     def current_player(self):
         """
         現在のプレイヤーを取得
         """
-        return self.black_player if self.current == 'black' else self.white_player
+        return self.black_player if self.current == "black" else self.white_player
 
     def update(self):
         """
@@ -64,8 +65,14 @@ class Othello:
 
         r_idx, c_idx = get_indices(pyxel.mouse_x, pyxel.mouse_y)
         # 配置できる場所だったらマスの色を変える
-        if r_idx is not None and c_idx is not None and self.board.can_move(ROWS[r_idx], COLS[c_idx], self.current):
-            pyxel.rect(SQUARE_SIZE * r_idx, SQUARE_SIZE * c_idx, SQUARE_SIZE, SQUARE_SIZE, 9)
+        if (
+            r_idx is not None
+            and c_idx is not None
+            and self.board.can_move(ROWS[r_idx], COLS[c_idx], self.current)
+        ):
+            pyxel.rect(
+                SQUARE_SIZE * r_idx, SQUARE_SIZE * c_idx, SQUARE_SIZE, SQUARE_SIZE, 9
+            )
 
         # 線の描画
         for i in range(len(ROWS) + 1):
@@ -77,21 +84,37 @@ class Othello:
             for y, col in enumerate(COLS):
                 color = self.board.get(row, col)
                 if color:
-                    code = 0 if color == 'black' else 7
-                    pyxel.circ(SQUARE_SIZE * x + SQUARE_SIZE // 2,
-                               SQUARE_SIZE * y + SQUARE_SIZE // 2, 12, code)
+                    code = 0 if color == "black" else 7
+                    pyxel.circ(
+                        SQUARE_SIZE * x + SQUARE_SIZE // 2,
+                        SQUARE_SIZE * y + SQUARE_SIZE // 2,
+                        12,
+                        code,
+                    )
 
         # パスボタンの描画
         can_pass = self.winner is None and self.board.can_pass(self.current)
-        pyxel.rect(PASS_BTN_X, PASS_BTN_Y, PASS_BTN_W, PASS_BTN_H, 9 if can_pass else 13)
-        pyxel.text(PASS_BTN_X + 10, PASS_BTN_Y + 3, 'PASS', 7)
+        pyxel.rect(
+            PASS_BTN_X, PASS_BTN_Y, PASS_BTN_W, PASS_BTN_H, 9 if can_pass else 13
+        )
+        pyxel.text(PASS_BTN_X + 10, PASS_BTN_Y + 3, "PASS", 7)
         # 現在のプレイヤー表示
-        pyxel.text(4, SCREEN_WIDTH + 5, "Black's turn" if self.current == 'black' else "White's turn", 7)
+        pyxel.text(
+            4,
+            SCREEN_WIDTH + 5,
+            "Black's turn" if self.current == "black" else "White's turn",
+            7,
+        )
         # ゲーム終了メッセージの描画
         if self.winner:
-            color = 'Black' if self.winner == 'black' else 'White'
-            pyxel.text(SCREEN_WIDTH // 2 - 34, SCREEN_WIDTH + 5, "{} player wins!".format(color), 14)
+            color = "Black" if self.winner == "black" else "White"
+            pyxel.text(
+                SCREEN_WIDTH // 2 - 34,
+                SCREEN_WIDTH + 5,
+                "{} player wins!".format(color),
+                14,
+            )
 
 
 if __name__ == "__main__":
-    Othello()
+    Reversi()
